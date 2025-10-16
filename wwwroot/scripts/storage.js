a6ed603831dbe5e37a36a84bf94f10580c049ead
Shahzad Khan <shahzy@msn.com>
2025-10-16 17:43:56 -0400

    cleanup

---
+++ wwwroot/scripts/storage.js
@@ -0,0 +1,17 @@
+﻿window.tabStorage = {
+    set: function (key, value) {
+        localStorage.setItem(key, value);
+    },
+    get: function (key) {
+        return localStorage.getItem(key);
+    }
+};
+
+window.themeStorage = {
+    set: function (value) {
+        localStorage.setItem("apexlm-theme", value);
+    },
+    get: function () {
+        return localStorage.getItem("apexlm-theme");
+    }
+};
